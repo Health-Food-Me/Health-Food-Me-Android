0d1edff449d89101f5c81d5e17eda44fb3e57460
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-05 04:03:23 +0900

    [add] #1 - build.gradle(app) 추가

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-parcelize")
+    kotlin("android")
+    kotlin("kapt")
+    kotlin("plugin.serialization") version "1.6.10"
+    id("androidx.navigation.safeargs.kotlin")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    defaultConfig {
+        applicationId = "org.helfoome"
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+    namespace = "org.helfoome"
+}
+
+dependencies {
+    implementation(libs.bundles.androidx)
+    implementation(libs.bundles.kotlin)
+    implementation(libs.bundles.dagger)
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.coil.core)
+    implementation(libs.okhttp.bom)
+    implementation(libs.okhttp.loggingInterceptor)
+    implementation(libs.timber)
+    implementation(libs.leakCanary)
+    implementation(libs.lottie)
+    implementation(libs.kotlin.serialization.converter)
+    implementation(libs.junit)
+    kapt(libs.bundles.compiler)
+}
