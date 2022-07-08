00de640a6141da857ac4ffd805e12c185a0ffe94
lee989898 <wnsrud1352@gmail.com>
2022-07-08 17:18:14 +0900

    [feat] #7 위치 권환 허용 여부 구현

--- app/src/main/java/org/helfoome/presentation/MainActivity.kt
+++ app/src/main/java/org/helfoome/presentation/MainActivity.kt
@@ -1,6 +1,11 @@
 package org.helfoome.presentation
 
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
@@ -10,16 +15,47 @@ import org.helfoome.databinding.ActivityMainBinding
 import org.helfoome.util.binding.BindingActivity
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main), OnMapReadyCallback {
+    private lateinit var naverMap: NaverMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initNaverMap()
+        requirePermission()
+    }
+
+    private fun requirePermission() {
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    initNaverMap()
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    initNaverMap()
+                }
+                else -> {
+                    initNaverMap()
+                    Toast.makeText(this, "위치 권한이 없어 현재 위치를 알 수 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
     }
 
     private fun initNaverMap() {
         val fm = supportFragmentManager
         val mapFragment = fm.findFragmentById(R.id.fragment_naver_map) as MapFragment?
             ?: MapFragment.newInstance().also {
-                fm.beginTransaction().add(R.id.fragment_naver_map, it).commit()
+                fm.commit {
+                    add<MapFragment>(R.id.fragment_naver_map)
+                    setReorderingAllowed(true)
+                }
             }
         mapFragment.getMapAsync(this)
     }
@@ -40,9 +76,5 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         val initialPosition = LatLng(37.5670135, 126.9783740)
         val cameraUpdate = CameraUpdate.scrollTo(initialPosition)
         naverMap.moveCamera(cameraUpdate)
-    }
-
-    companion object {
-        lateinit var naverMap: NaverMap
     }
 }
