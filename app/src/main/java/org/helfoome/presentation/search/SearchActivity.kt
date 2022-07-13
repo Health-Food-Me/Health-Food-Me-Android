cb483897535215933486350e2a8a0766ee247624
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-14 02:51:46 +0900

    [add] #35 - 서치 뷰 어댑터 로직 초기화

--- app/src/main/java/org/helfoome/presentation/search/SearchActivity.kt
+++ app/src/main/java/org/helfoome/presentation/search/SearchActivity.kt
@@ -8,37 +8,67 @@ import androidx.activity.viewModels
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ConcatAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import org.helfoome.R
 import org.helfoome.databinding.ActivitySearchBinding
+import org.helfoome.presentation.search.adapter.*
 import org.helfoome.presentation.search.type.SearchMode
 import org.helfoome.util.binding.BindingActivity
 import org.helfoome.util.ext.closeKeyboard
-import timber.log.Timber
+import org.helfoome.util.ext.showKeyboard
+import org.helfoome.util.ext.showToast
 
 @AndroidEntryPoint
 class SearchActivity : BindingActivity<ActivitySearchBinding>(R.layout.activity_search) {
     private val viewModel: SearchViewModel by viewModels()
+    private val autoCompleteAdapter = AutoCompleteAdapter()
+    private val recentAdapter = RecentAdapter()
+    private val resultAdapter = ResultAdapter()
+    private val searchMapTopAdapter = SearchMapTopAdapter()
+    private val searchRecentTopAdapter = SearchRecentTopAdapter()
+
+    private val recentConcatAdapter = ConcatAdapter(
+        searchRecentTopAdapter,
+        recentAdapter
+    )
+
+    private val resultConcatAdapter = ConcatAdapter(
+        searchMapTopAdapter,
+        resultAdapter
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        openKeyboard()
         initClickEvent()
         initTextChangeEvent()
         observeData()
     }
 
+    private fun openKeyboard() {
+        binding.etSearch.requestFocus()
+        showKeyboard(binding.etSearch)
+    }
+
     private fun initClickEvent() {
         with(binding) {
             with(viewModel) {
-                ibBack.setOnClickListener {
+                btnBack.setOnClickListener {
                     startSearchModeBackEvent(searchMode.value)
+                }
+
+                etSearch.setOnClickListener {
+                    if (searchMode.value == SearchMode.RESULT) {
+                        setSearchMode(SearchMode.AUTO_COMPLETE)
+                    }
                 }
             }
 
-            ibDelete.setOnClickListener {
+            btnDelete.setOnClickListener {
                 etSearch.text.clear()
             }
         }
@@ -60,7 +90,7 @@ class SearchActivity : BindingActivity<ActivitySearchBinding>(R.layout.activity_
 
     private fun initTextChangeEvent() {
         binding.etSearch.addTextChangedListener {
-            if(it.isNullOrEmpty())
+            if (it.isNullOrEmpty())
                 viewModel.setSearchMode(SearchMode.RECENT)
             else
                 viewModel.setSearchMode(SearchMode.AUTO_COMPLETE)
@@ -68,41 +98,45 @@ class SearchActivity : BindingActivity<ActivitySearchBinding>(R.layout.activity_
     }
 
     private fun observeData() {
-        viewModel.searchMode.flowWithLifecycle(lifecycle)
-            .onEach {
-                when (it) {
-                    SearchMode.RECENT -> {
-                        // TODO : 로컬데이터 불러오기
-                        binding.etSearch.isEnabled = true
+        with(binding) {
+            viewModel.searchMode.flowWithLifecycle(lifecycle)
+                .onEach {
+                    when (it) {
+                        SearchMode.RECENT -> {
+                            // TODO : 로컬데이터 불러오기
+                            rvSearch.adapter = recentConcatAdapter
+                        }
+                        SearchMode.AUTO_COMPLETE -> {
+                            // TODO : 자동완성 서버 통신
+                            rvSearch.adapter = autoCompleteAdapter
+                        }
+                        else -> {
+                            // TODO : 서버 통신 받아온 리스트 띄우기
+                            rvSearch.adapter = resultConcatAdapter
+                            binding.etSearch.clearFocus()
+                        }
                     }
-                    SearchMode.AUTO_COMPLETE -> {
-                        // TODO : 자동완성 서버 통신
-                        binding.etSearch.isEnabled = true
-                    }
-                    else -> {
-                        // TODO : 서버 통신 받아온 리스트 띄우기
-                        binding.etSearch.isEnabled = false
-                    }
-                }
-            }.launchIn(lifecycleScope)
+                }.launchIn(lifecycleScope)
 
-        viewModel.searchUiState.flowWithLifecycle(lifecycle)
-            .onEach {
-                when (it) {
-                    is SearchViewModel.SearchUiState.RecentSearch -> {
-
+            viewModel.searchUiState.flowWithLifecycle(lifecycle)
+                .onEach {
+                    when (it) {
+                        is SearchViewModel.SearchUiState.RecentSearch -> {
+                            recentAdapter.submitList(it.data)
+                        }
+                        is SearchViewModel.SearchUiState.AutoCompleteSearch -> {
+                            autoCompleteAdapter.submitList(it.data)
+                        }
+                        is SearchViewModel.SearchUiState.Result -> {
+                            resultAdapter.submitList(it.data)
+                        }
+                        else -> {
+                            // TODO : Loading, Error 추후 구현
+                            showToast("알 수 없는 에러")
+                        }
                     }
-                    is SearchViewModel.SearchUiState.AutoCompleteSearch -> {
-
-                    }
-                    is SearchViewModel.SearchUiState.Result ->
-                        Toast.makeText(this, "알 수 없는 에러", Toast.LENGTH_SHORT).show()
-                    else -> {
-                        // TODO : Loading, Error 추후 구현
-                    }
-                }
-            }.launchIn(lifecycleScope)
-
+                }.launchIn(lifecycleScope)
+        }
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
