7188e2e89fafe27451575d5a2644df712b4a2673
youngjinc <cyjin6789@gmail.com>
2022-07-13 14:47:16 +0900

    [chore] #19 - reformat 적용

--- app/src/main/java/org/helfoome/presentation/MainActivity.kt
+++ app/src/main/java/org/helfoome/presentation/MainActivity.kt
@@ -49,7 +49,7 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
-        grantResults: IntArray
+        grantResults: IntArray,
     ) {
         if (locationSource.onRequestPermissionsResult(
                 requestCode, permissions,
@@ -144,7 +144,7 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     }
 
     override fun onBackPressed() {
-        when(behavior.state) {
+        when (behavior.state) {
             BottomSheetBehavior.STATE_COLLAPSED -> behavior.state = BottomSheetBehavior.STATE_HIDDEN
             BottomSheetBehavior.STATE_EXPANDED -> behavior.state = BottomSheetBehavior.STATE_COLLAPSED
             BottomSheetBehavior.STATE_HIDDEN -> super.onBackPressed()
