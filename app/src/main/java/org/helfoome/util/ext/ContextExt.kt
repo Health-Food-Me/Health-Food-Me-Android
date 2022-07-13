06ad429cffe7bf4a54d921b4057fc41d18b23be4
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-13 09:40:27 +0900

    [add] #35 - ContextExt(키보드 유틸, 인텐트 관련 함수) 추가

---
+++ app/src/main/java/org/helfoome/util/ext/ContextExt.kt
@@ -0,0 +1,30 @@
+package org.helfoome.util.ext
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.os.bundleOf
+
+fun Context.showKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.showSoftInput(view.findFocus(), 0)
+}
+
+fun Context.closeKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+inline fun <reified T : Activity> Context.buildIntent(
+    vararg argument: Pair<String, Any?>
+) = Intent(this, T::class.java).apply {
+    putExtras(bundleOf(*argument))
+}
+
+inline fun <reified T : Activity> Context.startActivity(
+    vararg argument: Pair<String, Any?>
+) {
+    startActivity(buildIntent<T>(*argument))
+}
