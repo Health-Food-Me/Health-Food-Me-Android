2b6ff835ac10d9b9557e50d84e4a2ef33ae341f9
youngjinc <cyjin6789@gmail.com>
2022-07-15 04:40:37 +0900

    [ui] #51 - 리뷰 탭 아이템 구분선 추가

--- app/src/main/java/org/helfoome/presentation/restaurant/RestaurantReviewTabFragment.kt
+++ app/src/main/java/org/helfoome/presentation/restaurant/RestaurantReviewTabFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.google.android.material.tabs.TabLayout
+import dagger.hilt.android.AndroidEntryPoint
 import org.helfoome.R
 import org.helfoome.databinding.FragmentReviewBinding
+import org.helfoome.util.ItemDecorationUtil
 import org.helfoome.util.binding.BindingFragment
 
+@AndroidEntryPoint
 class RestaurantReviewTabFragment : BindingFragment<FragmentReviewBinding>(R.layout.fragment_review) {
     private val viewModel: RestaurantReviewViewModel by viewModels()
     private val restaurantGeneralReviewAdapter = RestaurantGeneralReviewAdapter()
@@ -21,7 +24,10 @@ class RestaurantReviewTabFragment : BindingFragment<FragmentReviewBinding>(R.lay
     }
 
     private fun initView() {
-        binding.reviewList.adapter = restaurantGeneralReviewAdapter
+        binding.reviewList.apply {
+            adapter = restaurantGeneralReviewAdapter
+            addItemDecoration(ItemDecorationUtil.ItemDecoration(1f, 72f, context.getColor(R.color.gray_500), 28))
+        }
         binding.layoutReviewTab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 when (tab?.position) {
