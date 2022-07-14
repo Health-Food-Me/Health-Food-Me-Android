1819e63f4657ce3d6c6252bd05287a01ef810813
youngjinc <cyjin6789@gmail.com>
2022-07-15 01:12:12 +0900

    [mod] #51 - title -> nickname으로 프로퍼티명 변경

--- app/src/main/java/org/helfoome/domain/entity/ReviewInfo.kt
+++ app/src/main/java/org/helfoome/domain/entity/ReviewInfo.kt
@@ -2,7 +2,7 @@ package org.helfoome.domain.entity
 
 data class ReviewInfo(
     val id: Int,
-    val title: String,
+    val nickname: String,
     val score: Float,
     val tags: List<String>,
     val description: String,
