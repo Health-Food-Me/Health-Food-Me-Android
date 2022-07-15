ae9b0aa33a8edd160f05c91b264c2aa123205912
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-15 22:50:07 +0900

    [feat] #64 - MyScrapAdapter 데이터 바인딩 연동

---
+++ app/src/main/java/org/helfoome/presentation/drawer/adapter/MyScrapAdapter.kt
@@ -0,0 +1,43 @@
+package org.helfoome.presentation.drawer.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import org.helfoome.databinding.ItemScrapBinding
+import org.helfoome.domain.entity.ScrapInfo
+import org.helfoome.util.ItemDiffCallback
+
+class MyScrapAdapter : ListAdapter<ScrapInfo, MyScrapAdapter.MyScrapViewHolder>(
+    ItemDiffCallback<ScrapInfo>(
+        onContentsTheSame = { old, new -> old == new },
+        onItemsTheSame = { old, new -> old.id == new.id }
+    )
+) {
+    private lateinit var inflater: LayoutInflater
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyScrapViewHolder {
+        if (!::inflater.isInitialized)
+            inflater = LayoutInflater.from(parent.context)
+
+        val binding = ItemScrapBinding.inflate(inflater, parent, false)
+        return MyScrapViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyScrapViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    class MyScrapViewHolder(private val binding: ItemScrapBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(scrapData: ScrapInfo) {
+            with(binding) {
+                ivScrap.isSelected = scrapData.isBookmarked
+                data = scrapData
+
+                ivScrap.setOnClickListener {
+                    it.isSelected = !it.isSelected
+                }
+            }
+        }
+    }
+}
