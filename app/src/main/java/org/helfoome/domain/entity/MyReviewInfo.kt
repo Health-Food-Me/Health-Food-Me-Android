66d2c936f4b88bbaf36c2fc0bc33e5bb0227f033
mdb1217 <wnsrud1352@gmail.com>
2022-07-17 00:44:43 +0900

    [mod] #79 - ktlint 수정

--- app/src/main/java/org/helfoome/domain/entity/MyReviewInfo.kt
+++ app/src/main/java/org/helfoome/domain/entity/MyReviewInfo.kt
@@ -3,7 +3,7 @@ package org.helfoome.domain.entity
 data class MyReviewInfo(
     val id: Int,
     val name: String,
-    val score: Int,
+    val score: Float,
     val hashtag: List<String>,
     val content: String,
     val image: List<String>,
