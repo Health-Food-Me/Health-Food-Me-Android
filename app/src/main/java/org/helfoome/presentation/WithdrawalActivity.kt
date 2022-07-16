8e7f48f86a0859a6540cc2361a27123b3085117c
mdb1217 <wnsrud1352@gmail.com>
2022-07-17 07:13:03 +0900

    [feat] #98 - 옆으로 드로워 레이아웃 제스처 제거

--- app/src/main/java/org/helfoome/presentation/WithdrawalActivity.kt
+++ app/src/main/java/org/helfoome/presentation/WithdrawalActivity.kt
@@ -4,22 +4,36 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import org.helfoome.R
 import org.helfoome.databinding.ActivityWithdrawalBinding
-import org.helfoome.databinding.WithdrawDialogBinding
+import org.helfoome.databinding.DialogWithdrawBinding
 import org.helfoome.util.DialogUtil
+import org.helfoome.util.ResolutionMetrics
 import org.helfoome.util.binding.BindingActivity
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class WithdrawalActivity : BindingActivity<ActivityWithdrawalBinding>(R.layout.activity_withdrawal) {
+    @Inject
+    lateinit var resolutionMetrics: ResolutionMetrics
     private val withdrawalViewModel: WithdrawalViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = withdrawalViewModel
 
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.ivBack.setOnClickListener {
+            finish()
+        }
+
         binding.btConfirm.setOnClickListener {
-            val bind: WithdrawDialogBinding = WithdrawDialogBinding.inflate(LayoutInflater.from(this@WithdrawalActivity))
-            val dialog = DialogUtil.makeDialog(this, bind, 288, 222)
+            val bind = DialogWithdrawBinding.inflate(LayoutInflater.from(this@WithdrawalActivity))
+            val dialog = DialogUtil.makeDialog(this, bind, resolutionMetrics.toPixel(288), resolutionMetrics.toPixel(222))
 
             bind.btnYes.setOnClickListener {
                 startActivity(Intent(this@WithdrawalActivity, LoginActivity::class.java))
