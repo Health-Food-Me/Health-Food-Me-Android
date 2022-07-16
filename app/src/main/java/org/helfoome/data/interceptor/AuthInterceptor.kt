cb4d245fe019c8394644f20f3f020df71233b717
mdb1217 <wnsrud1352@gmail.com>
2022-07-17 03:59:08 +0900

    [init] #63 - 서버 기초 세팅

---
+++ app/src/main/java/org/helfoome/data/interceptor/AuthInterceptor.kt
@@ -0,0 +1,20 @@
+package org.helfoome.data.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import org.helfoome.data.local.HFMSharedPreference
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val storage: HFMSharedPreference
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val requestBuilder = chain.request().newBuilder()
+        storage.accessToken.let {
+            requestBuilder.addHeader("Authorization", "Bearer $it")
+        }
+        return chain.proceed(requestBuilder.build())
+    }
+}
