55ef6d231266f0243eee23d87852582e7c2a4701
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-17 01:53:40 +0900

    [chore] #77 - 스크랩 패키지 이동 및 맵 선택 액티비티 추가

---
+++ app/src/main/java/org/helfoome/presentation/scrap/MapSelectActivity.kt
@@ -0,0 +1,12 @@
+package org.helfoome.presentation.scrap
+
+import android.os.Bundle
+import org.helfoome.R
+import org.helfoome.databinding.ActivityMapSelectBinding
+import org.helfoome.util.binding.BindingActivity
+
+class MapSelectActivity : BindingActivity<ActivityMapSelectBinding>(R.layout.activity_map_select) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
