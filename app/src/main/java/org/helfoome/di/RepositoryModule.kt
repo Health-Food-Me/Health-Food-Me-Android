57126a07fee4c99c866d4689637893f0624e9dff
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-19 07:00:23 +0900

    [add] #118 - 모듈 의존성 추가

--- app/src/main/java/org/helfoome/di/RepositoryModule.kt
+++ app/src/main/java/org/helfoome/di/RepositoryModule.kt
@@ -5,11 +5,12 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.helfoome.data.local.dao.SearchDao
-import org.helfoome.data.repository.SearchRepositoryImpl
 import org.helfoome.data.repository.LoginRepositoryImpl
+import org.helfoome.data.repository.SearchRepositoryImpl
 import org.helfoome.data.service.AuthService
-import org.helfoome.domain.repository.SearchRepository
+import org.helfoome.data.service.SearchService
 import org.helfoome.domain.repository.LoginRepository
+import org.helfoome.domain.repository.SearchRepository
 import javax.inject.Singleton
 
 @Module
@@ -17,8 +18,8 @@ import javax.inject.Singleton
 object RepositoryModule {
     @Provides
     @Singleton
-    fun provideSearchRepository(searchDao: SearchDao): SearchRepository =
-        SearchRepositoryImpl(searchDao)
+    fun provideSearchRepository(searchDao: SearchDao, searchService: SearchService): SearchRepository =
+        SearchRepositoryImpl(searchDao, searchService)
 
     @Singleton
     @Provides
--- app/src/main/java/org/helfoome/di/ServiceModule.kt
+++ app/src/main/java/org/helfoome/di/ServiceModule.kt
@@ -5,6 +5,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.helfoome.data.service.AuthService
+import org.helfoome.data.service.SearchService
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
@@ -16,5 +17,11 @@ object ServiceModule {
     @Provides
     fun provideAuthAPIService(retrofit: Retrofit): AuthService {
         return retrofit.create(AuthService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideSearchAPIService(retrofit: Retrofit): SearchService {
+        return retrofit.create(SearchService::class.java)
     }
 }
