9669d45df14599f62f988dad1adaef896af0eb62
youngjinc <cyjin6789@gmail.com>
2022-07-18 23:57:03 +0900

    [chore] #106 - ktlint 수정

--- app/src/main/java/org/helfoome/di/ServiceModule.kt
+++ app/src/main/java/org/helfoome/di/ServiceModule.kt
@@ -12,7 +12,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object ServiceModule {
-
     @Singleton
     @Provides
     fun provideAuthAPIService(retrofit: Retrofit): AuthService {
--- app/src/main/java/org/helfoome/domain/repository/RestaurantRepository.kt
+++ app/src/main/java/org/helfoome/domain/repository/RestaurantRepository.kt
@@ -3,6 +3,6 @@ package org.helfoome.domain.repository
 import org.helfoome.domain.entity.RestaurantInfo
 
 interface RestaurantRepository {
-    suspend fun fetchRestaurantSummary(restaurantId: String,userId: String): RestaurantInfo?
+    suspend fun fetchRestaurantSummary(restaurantId: String, userId: String): RestaurantInfo?
     suspend fun fetchRestaurantDetail(restaurantId: String): RestaurantInfo?
 }
