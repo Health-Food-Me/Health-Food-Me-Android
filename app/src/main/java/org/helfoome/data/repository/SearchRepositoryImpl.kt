eadb36687152ec89976d20e07ce4241a593931e0
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-19 07:01:17 +0900

    [add] #118 - search 레포지토리 추가

--- app/src/main/java/org/helfoome/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/org/helfoome/data/repository/SearchRepositoryImpl.kt
@@ -5,11 +5,13 @@ import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.flow
 import org.helfoome.data.local.dao.SearchDao
 import org.helfoome.data.local.entity.SearchData
+import org.helfoome.data.service.SearchService
 import org.helfoome.domain.entity.RecentSearchInfo
 import org.helfoome.domain.repository.SearchRepository
 
 class SearchRepositoryImpl(
-    private val searchDao: SearchDao
+    private val searchDao: SearchDao,
+    private val searchService: SearchService
 ) : SearchRepository {
     override fun getRecentKeyword(): Flow<List<RecentSearchInfo>> = flow {
         searchDao.getAll().buffer().collect {
@@ -24,4 +26,10 @@ class SearchRepositoryImpl(
     override suspend fun insertKeyword(item: RecentSearchInfo) = searchDao.insertKeyword(SearchData(item.keyword))
 
     override suspend fun removeKeyword(item: RecentSearchInfo) = searchDao.deleteKeyword(SearchData(item.keyword))
+
+    override suspend fun getSearchAutoComplete(query: String) = runCatching {
+        searchService.getSearchAutoComplete(query).data.map {
+            it.toAutoCompleteKeywordInfo()
+        }
+    }
 }
