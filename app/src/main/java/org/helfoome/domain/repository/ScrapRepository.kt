9576e1d90cbcdf310a713fc7a278d6282ef3d129
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-20 05:32:25 +0900

    [add] #137 - 스크랩 레포지토리 추가

---
+++ app/src/main/java/org/helfoome/domain/repository/ScrapRepository.kt
@@ -0,0 +1,8 @@
+package org.helfoome.domain.repository
+
+import org.helfoome.domain.entity.ScrapInfo
+
+interface ScrapRepository {
+    suspend fun getScrapList(userId: String): Result<List<ScrapInfo>>
+    suspend fun updateRestaurantScrap(restaurantId: String, userId: String): Boolean?
+}
