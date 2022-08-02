31895e7b26edc2da7b632fa74b3db08b63505f08
mdb1217 <wnsrud1352@gmail.com>
2022-08-02 14:38:02 +0900

    [feat] #226 - 스낵바 유틸로 변경

--- app/src/main/java/org/helfoome/presentation/drawer/ProfileModifyActivity.kt
+++ app/src/main/java/org/helfoome/presentation/drawer/ProfileModifyActivity.kt
@@ -9,32 +9,17 @@ import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import org.helfoome.R
 import org.helfoome.databinding.ActivityProfileModifyBinding
+import org.helfoome.util.SnackBarTopDown
 import org.helfoome.util.binding.BindingActivity
 
 @AndroidEntryPoint
 class ProfileModifyActivity : BindingActivity<ActivityProfileModifyBinding>(R.layout.activity_profile_modify) {
     private val viewModel: ProfileModifyViewModel by viewModels()
-    private var animation: Animation? = null
-    private var animator: Animation.AnimationListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding.viewModel = viewModel
 
-        animator = object : Animation.AnimationListener {
-            override fun onAnimationStart(animation: Animation?) = Unit
-            override fun onAnimationRepeat(animation: Animation?) = Unit
-            override fun onAnimationEnd(animation: Animation?) {
-                val bottomTopAnimation = AnimationUtils.loadAnimation(this@ProfileModifyActivity, R.anim.anim_snackbar_bottom_top)
-                binding.snvProfileModify.animation = bottomTopAnimation
-                if (viewModel.isOverlapNickName.value == false) {
-                    binding.snvProfileModify.setText("중복된 닉네임 입니다")
-                } else if (viewModel.isValidNickname.value == false) {
-                    binding.snvProfileModify.setText("닉네임 설정 기준에 적합하지 않습니다")
-                }
-            }
-        }
         initListener()
         initObserve()
     }
@@ -42,21 +27,13 @@ class ProfileModifyActivity : BindingActivity<ActivityProfileModifyBinding>(R.la
     private fun initObserve() {
         viewModel.isOverlapNickName.observe(this) {
             if (viewModel.isOverlapNickName.value == false) {
-                animation = AnimationUtils.loadAnimation(this, R.anim.anim_snackbar_top_down)
-                binding.snvProfileModify.animation = animation
-                binding.snvProfileModify.setText("중복된 닉네임 입니다")
-                animator?.let {
-                    animation?.setAnimationListener(it)
-                }
+                SnackBarTopDown.makeSnackBarTopDown(this, binding, R.id.snv_profile_modify, "중복된 닉네임 입니다")
             }
         }
 
         viewModel.isValidNickname.observe(this) {
             if (viewModel.isValidNickname.value == false) {
-                animation = AnimationUtils.loadAnimation(this, R.anim.anim_snackbar_top_down)
-                binding.snvProfileModify.animation = animation
-                binding.snvProfileModify.setText("닉네임 설정 기준에 적합하지 않습니다")
-                animator?.let { animation?.setAnimationListener(it) }
+                SnackBarTopDown.makeSnackBarTopDown(this, binding, R.id.snv_profile_modify, "닉네임 설정 기준에 적합하지 않습니다")
             }
         }
 
@@ -80,11 +57,5 @@ class ProfileModifyActivity : BindingActivity<ActivityProfileModifyBinding>(R.la
         binding.btModify.setOnClickListener {
             viewModel.profileModify()
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        animation?.cancel()
-        animation = null
     }
 }
