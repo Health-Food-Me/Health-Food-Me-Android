90fa2c7e1919f632d6f16e8127e5f9b9ed6f389b
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-08-09 23:01:21 +0900

    [FEAT] #202 - 스크랩 뷰모델 로직

--- app/src/main/java/org/helfoome/presentation/scrap/ScrapViewModel.kt
+++ app/src/main/java/org/helfoome/presentation/scrap/ScrapViewModel.kt
@@ -1,5 +1,7 @@
 package org.helfoome.presentation.scrap
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -8,15 +10,25 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import org.helfoome.data.local.HFMSharedPreference
+import org.helfoome.domain.entity.MarkerInfo
 import org.helfoome.domain.entity.ScrapInfo
 import org.helfoome.domain.repository.ScrapRepository
+import org.helfoome.util.ext.markerFilter
 import javax.inject.Inject
 
 @HiltViewModel
 class ScrapViewModel @Inject constructor(
     private val storage: HFMSharedPreference,
-    private val scrapRepository: ScrapRepository
+    private val scrapRepository: ScrapRepository,
 ) : ViewModel() {
+    // TODO : 초기화 로직 바꿀필요 있음(Sealed Class로)
+    private val _scrapMarkerList = MutableStateFlow<List<MarkerInfo>>(listOf())
+    val scrapMarkerList: StateFlow<List<MarkerInfo>>
+        get() = _scrapMarkerList.asStateFlow()
+
+    private val _location = MutableLiveData<List<MarkerInfo>>()
+    val location: LiveData<List<MarkerInfo>> = _location
+
     private val _scrapUiState =
         MutableStateFlow<ScrapUiState>(ScrapUiState.Loading)
     val scrapUiState: StateFlow<ScrapUiState>
@@ -28,13 +40,25 @@ class ScrapViewModel @Inject constructor(
                 .onSuccess {
                     if (it.isEmpty())
                         _scrapUiState.value = ScrapUiState.Empty
-                    else
+                    else {
                         _scrapUiState.value = it.toScrapUiState()
+                        _location.value?.let { markerInfoList ->
+                            _scrapMarkerList.value = markerInfoList.markerFilter(
+                                it.map {
+                                    it.id
+                                }
+                            )
+                        }
+                    }
                 }
                 .onFailure {
                     _scrapUiState.value = ScrapUiState.Error(it.message)
                 }
         }
+    }
+
+    fun setMapInfo(markerInfo: ArrayList<MarkerInfo>) {
+        _location.value = markerInfo
     }
 
     fun putScrap(restaurantId: String) {
