b5798595303b810c2a9aabdea63a045b8b63c7c3
mdb1217 <wnsrud1352@gmail.com>
2022-08-13 00:31:47 +0900

    [mod] #237 - ktlint 수정

--- app/src/main/java/org/helfoome/presentation/MainActivity.kt
+++ app/src/main/java/org/helfoome/presentation/MainActivity.kt
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager
 import android.graphics.Paint
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
