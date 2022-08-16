c612f73742df89690a8fe2490d0261c5f636b331
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-08-16 22:29:58 +0900

    [chore] #242 - RepositoryImpl 객체에 Inject constructor 추가

--- app/src/main/java/org/helfoome/data/repository/MapRepositoryImpl.kt
+++ app/src/main/java/org/helfoome/data/repository/MapRepositoryImpl.kt
@@ -4,8 +4,9 @@ import org.helfoome.data.model.response.BaseResponse
 import org.helfoome.data.model.response.ResponseMap
 import org.helfoome.data.service.MapService
 import org.helfoome.domain.repository.MapRepository
+import javax.inject.Inject
 
-class MapRepositoryImpl(private val mapService: MapService) : MapRepository {
+class MapRepositoryImpl @Inject constructor(private val mapService: MapService) : MapRepository {
     override suspend fun getMap(longitude: Double, latitude: Double, zoom: Long, category: String?): BaseResponse<List<ResponseMap>> {
         return mapService.getMapLocation(longitude, latitude, zoom, category)
     }
