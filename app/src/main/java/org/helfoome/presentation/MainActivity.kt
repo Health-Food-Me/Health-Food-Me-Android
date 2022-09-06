3fa8bd5b4812b1e67ca9b6156ecd415eda2a02ed
mdb1217 <wnsrud1352@gmail.com>
2022-09-06 11:43:28 +0900

    [chore] #260 MainActivity Unused import 제거

--- app/src/main/java/org/helfoome/presentation/MainActivity.kt
+++ app/src/main/java/org/helfoome/presentation/MainActivity.kt
@@ -6,13 +6,9 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Paint
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
-import android.view.WindowInsets
-import android.view.WindowManager
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
