45b6c97a8e981f33fd45152248fd0d3d8baa4169
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-09-19 21:06:08 +0900

    [add] #289 - replace 관련 확장함수 추가

---
+++ app/src/main/java/org/helfoome/util/ext/ActivityExt.kt
@@ -0,0 +1,14 @@
+package org.helfoome.util.ext
+
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+
+inline fun <reified T : Fragment> AppCompatActivity.replace(@IdRes frameId: Int) {
+    supportFragmentManager.commit {
+        replace<T>(frameId)
+        setReorderingAllowed(true)
+    }
+}
