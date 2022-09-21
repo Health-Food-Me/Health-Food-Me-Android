78f52539e51e1b53ba9567248d90f5e9380b3a8e
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-09-21 19:51:22 +0900

    [feat] #288 - 서치 레포지토리 수정

--- app/src/main/java/org/helfoome/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/org/helfoome/data/repository/SearchRepositoryImpl.kt
@@ -2,11 +2,12 @@ package org.helfoome.data.repository
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.buffer
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import org.helfoome.data.datasource.RemoteSearchDataSource
 import org.helfoome.data.local.dao.SearchDao
 import org.helfoome.data.local.entity.SearchData
 import org.helfoome.domain.entity.RecentSearchInfo
+import org.helfoome.domain.entity.SearchResultInfo
 import org.helfoome.domain.repository.SearchRepository
 import javax.inject.Inject
 
@@ -14,22 +15,22 @@ class SearchRepositoryImpl @Inject constructor(
     private val searchDao: SearchDao,
     private val searchDataSource: RemoteSearchDataSource,
 ) : SearchRepository {
-    override fun getRecentKeyword(): Flow<List<RecentSearchInfo>> = flow {
-        searchDao.getAll().buffer().collect {
-            emit(
-                it.map { data ->
-                    RecentSearchInfo(data.keyword)
-                }.reversed()
-            )
-        }
+    override fun getRecentKeyword(): Flow<List<RecentSearchInfo>> = searchDao.getAll().buffer().map {
+        it.map { data ->
+            RecentSearchInfo(data.keyword, data.isCategory)
+        }.reversed()
     }
 
-    override suspend fun insertKeyword(item: RecentSearchInfo) = searchDao.insertKeyword(SearchData(item.keyword))
+    override suspend fun insertKeyword(item: RecentSearchInfo) = searchDao.insertKeyword(SearchData(item.keyword, item.isCategory))
 
-    override suspend fun removeKeyword(item: RecentSearchInfo) = searchDao.deleteKeyword(SearchData(item.keyword))
+    override suspend fun removeKeyword(keyword: String) = searchDao.deleteKeyword(keyword)
 
-    override suspend fun getSearchAutoComplete(query: String) = runCatching {
-        searchDataSource.getSearchAutoComplete(query).data.map {
+    override suspend fun getSearchAutoComplete(longitude: Double, latitude: Double, query: String) = runCatching {
+        searchDataSource.getSearchAutoComplete(
+            longitude,
+            latitude,
+            query
+        ).data.map {
             it.toAutoCompleteKeywordInfo()
         }
     }
@@ -47,4 +48,15 @@ class SearchRepositoryImpl @Inject constructor(
             it.toSearchResultInfo()
         }
     }
+
+    override suspend fun getSearchCategoryCard(longitude: Double, latitude: Double, keyword: String): Result<List<SearchResultInfo>> =
+        runCatching {
+            searchDataSource.getSearchRestaurantCard(
+                longitude,
+                latitude,
+                keyword
+            ).data.map {
+                it.toSearchResultInfo()
+            }
+        }
 }
