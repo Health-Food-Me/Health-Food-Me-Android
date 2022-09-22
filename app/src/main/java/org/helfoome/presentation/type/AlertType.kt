13b5ee84c238322a478b5a1ed991feb32f886f8e
lee989898 <wnsrud1352@gmail.com>
2022-09-22 13:16:49 +0900

    [mod] #289 AlertType Pair<Int, Int>값 넣는 것으로 변경

--- app/src/main/java/org/helfoome/presentation/type/AlertType.kt
+++ app/src/main/java/org/helfoome/presentation/type/AlertType.kt
@@ -1,5 +1,10 @@
 package org.helfoome.presentation.type
 
-enum class AlertType {
-    EDIT_CANCEL, WRITE_CANCEL, LOGOUT, DELETE_REVIEW
+import org.helfoome.R
+
+enum class AlertType(val strResId: Pair<Int, Int>) {
+    EDIT_CANCEL(Pair(R.string.my_edit_dialog_caution, R.string.my_edit_dialog_caution_description)),
+    WRITE_CANCEL(Pair(R.string.logout_dialog_caution, R.string.my_review_write_dialog_caution_description)),
+    LOGOUT(Pair(R.string.logout_dialog_caution, R.string.logout_dialog_caution_description)),
+    DELETE_REVIEW(Pair(R.string.my_review_dialog_caution, 0)),
 }
