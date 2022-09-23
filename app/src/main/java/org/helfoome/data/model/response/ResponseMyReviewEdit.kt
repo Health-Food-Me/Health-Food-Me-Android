03b78aa0431c597e09131a28d67327393681175e
youngjinc <cyjin6789@gmail.com>
2022-09-23 11:09:11 +0900

    [mod] #326 리뷰 편집 response 수정

--- app/src/main/java/org/helfoome/data/model/response/ResponseMyReviewEdit.kt
+++ app/src/main/java/org/helfoome/data/model/response/ResponseMyReviewEdit.kt
@@ -5,23 +5,23 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ResponseMyReviewEdit(
-    @SerialName("__v")
-    val v: Int,
     @SerialName("_id")
     val id: String,
-    val content: String,
-    val good: List<String>,
-    val image: List<Image>,
+    val restaurantId: String,
     val restaurant: String,
+    val writerId: String,
+    val writer: String,
     val score: Double,
+    val content: String,
+    val image: List<Image>,
     val taste: String,
-    val writer: String
+    val good: List<String>,
 ) {
     @Serializable
     data class Image(
         @SerialName("_id")
         val id: String,
         val name: String,
-        val url: String
+        val url: String,
     )
 }
