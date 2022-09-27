ef354dfdbe078ef0cd9434740015ae93264c8578
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-09-28 03:06:13 +0900

    [add] #377 - 싱글 클릭리스너 추가

--- app/src/main/java/org/helfoome/util/ext/ViewExt.kt
+++ app/src/main/java/org/helfoome/util/ext/ViewExt.kt
@@ -3,6 +3,7 @@ package org.helfoome.util.ext
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
+import android.view.View
 import android.widget.TextView
 import org.helfoome.R
 import timber.log.Timber
@@ -20,6 +21,20 @@ fun TextView.setAutoKeyword(restaurant: String, keyword: String?) {
                     Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
                 )
             }
+        }
+    }
+}
+
+inline fun View.setOnSingleClickListener(
+    delay: Long = 500L,
+    crossinline block: (View) -> Unit
+) {
+    var previousClickedTime = 0L
+    setOnClickListener { view ->
+        val clickedTime = System.currentTimeMillis()
+        if (clickedTime - previousClickedTime >= delay) {
+            block(view)
+            previousClickedTime = clickedTime
         }
     }
 }
