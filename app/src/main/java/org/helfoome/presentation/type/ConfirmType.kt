9078e4a271159982c042f504079c6c35c273a1b4
lee989898 <wnsrud1352@gmail.com>
2022-10-04 19:11:28 +0900

    [mod] #401 ktlint 수정

--- app/src/main/java/org/helfoome/presentation/type/ConfirmType.kt
+++ app/src/main/java/org/helfoome/presentation/type/ConfirmType.kt
@@ -3,7 +3,7 @@ package org.helfoome.presentation.type
 import org.helfoome.R
 
 enum class ConfirmType(val dialogTitle: Int, val dialogDescription: Int) {
-    NETWORK_CONFIRM(R.string.confirm_dialog_network_caution,0),
+    NETWORK_CONFIRM(R.string.confirm_dialog_network_caution, 0),
     LOGIN_CONFIRM(R.string.confirm_dialog_login_caution, 0),
     LOCATION_CONFIRM(R.string.confirm_dialog_location_caution, R.string.confirm_dialog_location_caution_description)
 }
