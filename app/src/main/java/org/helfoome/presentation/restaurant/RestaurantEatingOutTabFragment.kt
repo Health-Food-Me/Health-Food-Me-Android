172d398abcde881510fc4089a010e78c1a052ba3
youngjinc <cyjin6789@gmail.com>
2022-10-07 19:11:11 +0900

    [chore] - #428 린트 체크

--- app/src/main/java/org/helfoome/presentation/restaurant/RestaurantEatingOutTabFragment.kt
+++ app/src/main/java/org/helfoome/presentation/restaurant/RestaurantEatingOutTabFragment.kt
@@ -7,7 +7,6 @@ import dagger.hilt.android.AndroidEntryPoint
 import org.helfoome.R
 import org.helfoome.databinding.FragmentEatingOutBinding
 import org.helfoome.presentation.MainViewModel
-import org.helfoome.presentation.restaurant.adapter.RestaurantMenuAdapter
 import org.helfoome.util.binding.BindingFragment
 
 @AndroidEntryPoint
