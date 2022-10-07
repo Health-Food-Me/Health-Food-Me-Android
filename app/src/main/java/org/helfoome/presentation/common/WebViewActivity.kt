e473495f995ba635c7d5582c998c57f08feaf3c4
youngjinc <cyjin6789@gmail.com>
2022-10-07 20:24:48 +0900

    [feat] - #438 웹뷰 클릭 이벤트 처리

--- app/src/main/java/org/helfoome/presentation/common/WebViewActivity.kt
+++ app/src/main/java/org/helfoome/presentation/common/WebViewActivity.kt
@@ -3,6 +3,7 @@ package org.helfoome.presentation.common
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import dagger.hilt.android.AndroidEntryPoint
 import org.helfoome.R
@@ -11,16 +12,18 @@ import org.helfoome.util.binding.BindingActivity
 
 @AndroidEntryPoint
 class WebViewActivity : BindingActivity<ActivityWebViewBinding>(R.layout.activity_web_view) {
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initView()
+        initListeners()
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun initView() {
-        binding.webView.apply {
+        webView = binding.webView.apply {
             // 새 창 띄우지 않기
             webViewClient = WebViewClient()
             webChromeClient = WebChromeClient() // 크롬환경에 맞는 세팅을 해줌
@@ -35,6 +38,21 @@ class WebViewActivity : BindingActivity<ActivityWebViewBinding>(R.layout.activit
 
             intent.getStringExtra(ARG_WEB_VIEW_LINK)?.let { loadUrl(it) }
         }
+    }
+
+    private fun initListeners() {
+        binding.btnRefresh.setOnClickListener {
+            webView.reload()
+        }
+
+        binding.btnComplete.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        overridePendingTransition(0, 0)
     }
 
     companion object {
