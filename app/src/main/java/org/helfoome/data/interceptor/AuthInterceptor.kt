5594972323546f7bf44c0477690b20bf288ea55e
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-10-09 00:58:49 +0900

    [feat] #352 - AuthInterceptor 코드 리프레시 로직까지 추가

--- app/src/main/java/org/helfoome/data/interceptor/AuthInterceptor.kt
+++ app/src/main/java/org/helfoome/data/interceptor/AuthInterceptor.kt
@@ -1,22 +1,30 @@
 package org.helfoome.data.interceptor
 
+import android.app.Application
+import android.content.Intent
 import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 import org.helfoome.BuildConfig
+import org.helfoome.R
 import org.helfoome.data.local.HFMSharedPreference
 import org.helfoome.data.model.response.ResponseRefresh
+import org.helfoome.util.ext.showToast
 import javax.inject.Inject
 
 class AuthInterceptor @Inject constructor(
     private val storage: HFMSharedPreference,
-    private val gson: Gson
+    private val gson: Gson,
+    private val context: Application
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
-        val authRequest = if (isLogin(originalRequest)) originalRequest else
-            originalRequest.newBuilder().addHeader("token", storage.accessToken).build()
+        val authRequest = originalRequest.newBuilder().addHeader("token", storage.accessToken).build()
         val response = chain.proceed(authRequest)
 
         when (response.code) {
@@ -29,24 +37,36 @@ class AuthInterceptor @Inject constructor(
                 val refreshTokenResponse = chain.proceed(refreshTokenRequest)
 
                 if (refreshTokenResponse.isSuccessful) {
-                    val refreshToken = gson.fromJson(
+                    val responseRefresh = gson.fromJson(
                         refreshTokenResponse.body?.string(),
                         ResponseRefresh::class.java
                     )
                     with(storage) {
-                        accessToken = refreshToken.data.accessToken
-                        this.refreshToken = refreshToken.data.refreshToken
+                        accessToken = responseRefresh.data.accessToken
+                        refreshToken = responseRefresh.data.refreshToken
                     }
                     val newRequest = originalRequest.newBuilder()
                         .addHeader("token", storage.accessToken)
                         .build()
                     return chain.proceed(newRequest)
+                } else {
+                    with(context) {
+                        CoroutineScope(Dispatchers.Main).launch {
+                            startActivity(
+                                Intent.makeRestartActivityTask(
+                                    packageManager.getLaunchIntentForPackage(packageName)?.component
+                                )
+                            )
+                            showToast(getString(R.string.auto_login_fail))
+                            storage.clear()
+                            Runtime.getRuntime().exit(0)
+                            cancel()
+                        }
+                    }
                 }
             }
         }
+
         return response
     }
-
-    private fun isLogin(originalRequest: Request) =
-        !originalRequest.url.encodedPath.contains("token") && originalRequest.url.encodedPath.contains("auth")
 }
